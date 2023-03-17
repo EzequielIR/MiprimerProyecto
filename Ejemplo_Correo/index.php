0ce9c55eaf56000c86e747164a74b102c3e23e69
EzequielIR <eezequiel260@gmail.com>
2023-03-17 11:14:22 -0600

    Correo

---
+++ Ejemplo_Correo/index.php
@@ -0,0 +1,59 @@
+<?php
+$errores = '';
+$enviado = '';
+
+//comprobamos que el formulario haya sido enviado
+if (isset($_POST['submit'])) {
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
+    $mensaje = $_POST['mensaje'];
+
+    //Comprobamos que nombre no este vacio
+    if (!empty($nombre)) {
+        //Sanitizar el nombre para eliminar caracteres que no deberian estar
+        //filter_var(preg_replace("([^a-z|^A-Z])", "", htmlentities($fe)));
+        $nombre = trim($nombre);
+        $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+        //comprobamos que el nombre no quede vacio despues de sanitizar
+        if ($nombre == "") {
+            $errores .= 'Por favor ingresa un nombre.<br/>';
+        }
+    } else {
+        $errores .= 'Por favor ingresa un nombre.<br/s>';
+    }
+
+    if (!empty($correo)) {
+        $correo = filter_var($correo,FILTER_SANITIZE_EMAIL);
+        //Comprobamos que sea un correo valido
+        if (!filter_var($correo,FILTER_VALIDATE_EMAIL)) {
+            $errores.= "Por favor ingresa un correo valido.<br/>";
+        }
+    }else {
+        $errores.= 'Por favor ingresa un correo.<br/>';
+    }
+
+    if (!empty($mensaje)) {
+        //Podermos sanitizar con filter var
+        //Comvertir signos del mensaje en entidades HTML
+        $mensaje = htmlentities($mensaje);
+        $mensaje = trim($mensaje);
+        $mensaje = stripslashes($mensaje);
+    }else {
+        $errores.= 'Por favor ingresa el mensaje.<br/>';
+    }
+
+    //Comprovamos si hay errores antes de enviar
+    if (!$errores) {
+        $destino = $correo;
+        $asunto = 'Correo enviado desde miPagina.com';
+        $mensaje = 'Mensaje: '.$_POST['mensaje'];
+        $headers = "From: eezequiel260@gmail.com";
+        mail($destino,$asunto,$mensaje,$headers);
+        $enviado = 'true';
+        
+    }
+}
+
+require 'index.view.php';
+
+?>
---
+++ Ejemplo_Correo/index.view.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <title>Formulario de Contacto</title>
+</head>
+
+<body>
+    <div class="wrap">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+            <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre:" value="<?php if (!$enviado && isset($nombre)) {
+                                                                                                                echo $nombre;
+                                                                                                            } ?>">
+
+            <input type="text" class="form-control" name="correo" id="correo" placeholder="Correo:" value="<?php if (!$enviado && isset($correo)) {
+                                                                                                                echo $correo;
+                                                                                                            } ?>">
+
+            <textarea type="text" class="form-control" name="mensaje" id="mensaje"><?php if (!$enviado && isset($mensaje)) {
+                                                                                        echo $mensaje;
+                                                                                    } ?></textarea>
+
+            <?php if (!empty($errores)) { ?>
+                <div class="alert error" role="alert">
+                    <?php echo $errores; ?>
+                </div>
+            <?php } elseif ($enviado) { ?>
+                <div class="alert success" role="alert">
+                    <?php echo 'Enviado Correctamente'; ?>
+                </div>
+            <?php } ?>
+            <center>
+                <input type="submit" name="submit" class="btn btn-primary" value="Envias Correo">
+            </center>
+        </form>
+    </div>
+</body>
+
+</html>
