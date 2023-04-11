db72f0872f21baf34ea6ba40be74a0f5df577e1e
EzequielIR <eezequiel260@gmail.com>
2023-04-10 23:54:32 -0600

    Python

--- PHP_Conecctions/pdo.php
+++ PHP_Conecctions/pdo.php
@@ -1,3 +1,6 @@
+<html>
+    <h1>Datos de la base</h1>
+<html>
 <?php
     try {
         $conexion = new PDO('mysql:host=localhost;dbname=primero','root','');
@@ -7,6 +10,30 @@
         foreach ($resultado as $fila) {
             echo $fila['id'].".- ".$fila['nombre']."<br />";
         }
+        ?>
+        <html>
+            <hr>
+            <h1>BETWEEN 2-3</h1>
+        </html>
+        <?php
+        $res = $conexion->query("SELECT * FROM usuario where id BETWEEN 2 AND 3;");
+
+        foreach ($res as $fila) {
+            echo $fila['id'].".- ".$fila['nombre']."<br />";
+        }
+
+        ?>
+        <html>
+            <hr>
+            <h1>LIKE E%</h1>
+        </html>
+        <?php
+        $res = $conexion->query("SELECT * FROM usuario where nombre like 'E%';");
+
+        foreach ($res as $fila) {
+            echo $fila['id'].".- ".$fila['nombre']."<br />";
+        }
+
     }catch(PDOException $e){
         echo "Error: ".$e->getMessage();
     }
